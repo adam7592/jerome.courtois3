ea6ab788433cef3fd635c0e9cd13852083598054
Averon <77922089+TheAveron@users.noreply.github.com>
2024-06-13 20:18:38 +0200

    feat: completing homepage to add quotes and add credits

---
+++ assets/scripts/index.js
@@ -0,0 +1,9 @@
+function getTheme() {
+    if(window.matchMedia && window.matchMedia("(prefers-color-scheme:dark)").matches) {
+        document.documentElement.setAttribute("data-theme", "dark");
+    } else {
+        document.documentElement.setAttribute("data-theme", "light");
+    }
+}
+
+getTheme()
